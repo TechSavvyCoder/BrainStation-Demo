569b69bcbb773e3b5e2e7357bd990b4453ded550
Diego <diegopinlac@gmail.com>
2025-01-18 11:47:38 -0800

    Added Exercise 25

---
+++ 25 - Higher Order Functions/lab-higher-order-functions-starter/js/main.js
@@ -0,0 +1,73 @@
+/* --------------------------------------------------------
+ * Movie Data to use during lab
+ */
+const movies = [
+  {
+    title: 'Jurassic Park',
+    year: 1993,
+    genres: ['action', 'adventure', 'sci-fi'],
+  },
+  {
+    title: 'Back to the Future',
+    year: 1985,
+    genres: ['adventure', 'comedy', 'sci-fi'],
+  },
+  {
+    title: 'Aliens',
+    year: 1986,
+    genres: ['action', 'adventure', 'sci-fi'],
+  },
+  {
+    title: 'Indiana Jones and the Raiders of the Lost Ark',
+    year: 1981,
+    genres: ['action', 'adventure'],
+  },
+  {
+    title: 'Star Wars: Episode IV - A New Hope',
+    year: 1977,
+    genres: ['action', 'adventure', 'fantasy', 'sci-fi'],
+  },
+  {
+    title: 'Total Recall',
+    year: 1990,
+    genres: ['action', 'sci-fi', 'thriller'],
+  },
+  {
+    title: 'Mrs. Doubtfire',
+    year: 1993,
+    genres: ['comedy', 'drama', 'family'],
+  },
+  {
+    title: 'Hook',
+    year: 1991,
+    genres: ['adventure', 'comedy', 'family'],
+  },
+  {
+    title: 'Princess Mononoke',
+    year: 1997,
+    genres: ['animation', 'action', 'adventure'],
+  },
+  {
+    title: 'The Matrix',
+    year: 1999,
+    genres: ['action', 'sci-fi'],
+  },
+  {
+    title: 'Star Trek II: The Wrath of Khan',
+    year: 1982,
+    genres: ['action', 'adventure', 'sci-fi'],
+  },
+  {
+    title: 'Toy Story',
+    year: 1995,
+    genres: ['animation', 'adventure', 'comedy'],
+  },
+  {
+    title: 'I am Legend',
+    year: 2007,
+    genres: ['action', 'adventure', 'drama'],
+  },
+];
+
+// inspect movies array
+// console.table(movies);
